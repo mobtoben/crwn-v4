8f662a6f01fe2919c68ac366ea9a79145474d9b2
mobroben <rajakkertie@gmail.com>
2022-11-07 20:54:14 +0700

    migrating Sagas to TypeScript

--- src/store/categories/category.saga.ts
+++ src/store/categories/category.saga.ts
@@ -1,20 +1,20 @@
-import { takeLatest, all, call, put } from 'redux-saga/effects';
+import { takeLatest, all, call, put } from "typed-redux-saga/macro";
 
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 
 import {
   fetchCategoriesSuccess,
   fetchCategoriesFailed,
-} from './category.action';
+} from "./category.action";
 
-import { CATEGORIES_ACTION_TYPES } from './category.types';
+import { CATEGORIES_ACTION_TYPES } from "./category.types";
 
 export function* fetchCategoriesAsync() {
   try {
-    const categoriesArray = yield call(getCategoriesAndDocuments, 'categories');
+    const categoriesArray = yield* call(getCategoriesAndDocuments);
     yield put(fetchCategoriesSuccess(categoriesArray));
   } catch (error) {
-    yield put(fetchCategoriesFailed(error));
+    yield put(fetchCategoriesFailed(error as Error));
   }
 }
 
